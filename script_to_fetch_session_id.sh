a5f69784c042b7bb83d7001d7ebe6b73a8b2ca79
inderjitblaggan <36985197+inderjitblaggan@users.noreply.github.com>
2023-04-09 05:50:36 +0530

    Update script_to_fetch_session_id.sh

--- script_to_fetch_session_id.sh
+++ script_to_fetch_session_id.sh
@@ -4,5 +4,5 @@ tomcat_container_id=$(docker ps | grep 'catalina.sh run' | awk '{print $1}')
 timeout 10s docker exec $tomcat_container_id curl -s http://localhost:8080/common/SimpleSystemStatusServlet?admin_pw=teams | grep session_count  > temp.txt
 instance_id=$(curl -sS http://169.254.169.254/latest/meta-data/instance-id)
 sed -i "s/session_count/tomcat-session-count/g" temp.txt
-echo "{instance_id:$instance_id,tomcat-session-count=$(grep -oE '[0-9]+' temp.txt)}" > output.txt
+echo "{\"instance_id\":\"$instance_id\",\"tomcat-session-count\"=$(grep -oE '[0-9]+' temp.txt)}" > output.txt
 cat output.txt
